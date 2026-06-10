6de5ca1e0cd16022931a900a46a55358eaa19c8e
normanwaycalendar <tomas.furlong@wexfordcoco.ie>
2026-06-10 12:15:13 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const SUPABASE_URL = "https://zlldjmfzcuawrprsqvmn.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpsbGRqbWZ6Y3Vhd3JwcnNxdm1uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEwMDk5MDUsImV4cCI6MjA5NjU4NTkwNX0.QhUkbTA3q33QiCfywvZ6xbS4ru_InCdYfwZ_be6DSdM";
 const TABLE_NAME = "events";
 
-const supabase = window.supabase.createClient(
+const supabaseClient = window.supabase.createClient(
   "https://zlldjmfzcuawrprsqvmn.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpsbGRqbWZ6Y3Vhd3JwcnNxdm1uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEwMDk5MDUsImV4cCI6MjA5NjU4NTkwNX0.QhUkbTA3q33QiCfywvZ6xbS4ru_InCdYfwZ_be6DSdM"
 );
